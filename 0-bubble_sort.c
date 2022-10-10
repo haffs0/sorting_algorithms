6d2762d60d5dcda45aea340d02ee04773676ea07
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-10-10 09:55:36 +0000

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -34,11 +34,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = size - 1; j > i; j--)
+		for (j = 0; j < size - i - 1 ; j++)
 		{
-			compexch(array, j - 1, j);
+			compexch(array, j, j + 1);
 			print_array(array, size);
 		}
 	}
