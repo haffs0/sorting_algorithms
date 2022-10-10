3d629863b0bcd1f06fc027b7111ba47f1ed56bdf
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-10-10 10:12:09 +0000

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,25 +2,16 @@
 
 /**
  * compexch - compare and exchange the array content by index
- * @array: array to compare and exchange content
- * @l: first index
- * @h: second index
+ * @l: first pointer
+ * @h: second pointer
  * Return: nothing
  */
 
-void compexch(int *array, int l, int h)
+void compexch(int* l, int* h)
 {
-	int temp, idx1, idx2;
-
-	idx1 = *(array + l);
-	idx2 = *(array + h);
-
-	if (idx1 > idx2)
-	{
-		temp = idx1;
-		*(array + l) = idx2;
-		*(array + h) = temp;
-	}
+	int temp = *l;
+	*l = *h;
+	*h = temp;
 }
 
 /**
@@ -34,11 +25,15 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1 ; j++)
 		{
-			compexch(array, j, j + 1);
+			if (*(array + j) > *(array + j + 1))
+				compexch((array + j), (array + j + 1));
 			print_array(array, size);
 		}
 	}
